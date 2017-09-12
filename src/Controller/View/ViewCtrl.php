e77d4e54bfddd784b5c5586cf4c135666a39b453
Federico Ercole <sercinci@sercinci-XPS.lan>
2017-09-12 20:18:06 +0200

    new tournament layout

--- src/Controller/View/ViewCtrl.php
+++ src/Controller/View/ViewCtrl.php
@@ -48,23 +48,7 @@ class ViewCtrl extends BaseCtrl
 
     public function showTournaments($req, $res, $arg)
     {
-        $user = User::with(['teams' => function($query) {
-                $query->with(['tournament' => function($query) {
-                    $query->where('status', 'underway')
-                        ->select('id', 'title')
-                        ->get();
-                }]);
-            }])
-            ->find($this->userData->id);
-        if ($this->checkDate($user->updated_at)) {
-            $stats = SteamCtrl::stats($user->steam_id, $this->hybridConfig['Steam']['keys']['secret']);
-            //$steam = new SteamCtrl;
-            //$stats = $steam->stats($user->steam_id);
-            $user->mmr_dm = $stats['mmr_dm'];
-            $user->mmr_rm = $stats['mmr_rm'];
-            $user->games = $stats['games'];
-            $user->save();
-        }
+        $user = $this->getUserTournaments();
         $tournaments = Tournament::with(['teams' => function($query) {
                     $query->with(['members' => function($query) {
                         $query->select('id', 'username', 'slug', 'avatar', 'profileurl', 'mmr_dm', 'mmr_rm', 'games');
@@ -76,8 +60,12 @@ class ViewCtrl extends BaseCtrl
                 ->get();
         foreach ($tournaments as $tournament) {
             $tournament->joined = 0;
+            $tournament->subscribed = false;
             foreach ($user->teams as $t) {
-                $tournament->subscribed = $t->tournament_id == $tournament->id ? true : false;
+                if ($t->tournament_id == $tournament->id) {
+                    $tournament->subscribed = true;
+                    break;
+                }
             }
             foreach ($tournament->teams as $team) {
                 $tournament->joined += $team->members_count;
@@ -89,11 +77,31 @@ class ViewCtrl extends BaseCtrl
         ]);
     }
 
+    private function getUserTournaments()
+    {
+        $user = User::with(['teams' => function($query) {
+                $query->with(['tournament' => function($query) {
+                    $query->where('status', 'underway')
+                        ->select('id', 'title')
+                        ->get();
+                }]);
+            }])
+            ->find($this->userData->id);
+        if ($this->checkDate($user->updated_at)) {
+            $stats = SteamCtrl::stats($user->steam_id, $this->hybridConfig['Steam']['keys']['secret']);
+            $user->mmr_dm = $stats['mmr_dm'];
+            $user->mmr_rm = $stats['mmr_rm'];
+            $user->games = $stats['games'];
+            $user->save();
+        }
+        return $user;
+    }
+
     public function showNewTournament($req, $res, $arg)
     {
-        //$user = User::find($this->userData->id);
+        $user = $this->getUserTournaments();
         return $this->view->render($res, 'new_tournament.html.twig', [
-            'user' => true
+            'user' => $user
         ]);
     }
 
