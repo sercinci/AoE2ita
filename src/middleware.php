546465422fe76cb18e6ec48d65b2c67d39a385ee
Federico Ercole <federico_ercole91@yahoo.it>
2017-09-23 21:09:07 +0200

    texts, og tags, Fb buttons

--- src/middleware.php
+++ src/middleware.php
@@ -11,7 +11,11 @@ $app->add(new \Slim\Middleware\JwtAuthentication([
         $container["jwt"] = $arg["decoded"];
     },
     "error" => function($req, $res, $arg) {
-        return $res->withRedirect('/?auth=false', 401);
+        if(strpos($_SERVER["HTTP_USER_AGENT"], "facebookexternalhit/") !== false || strpos($_SERVER["HTTP_USER_AGENT"], "Facebot") !== false) {
+             return $next($req, $res, $arg);
+        } else {
+            return $res->withRedirect('/?auth=false', 401);
+        }
         //return $res->withJson(array('message' => 'Token expired'));
     },
     /*"rules" => [
